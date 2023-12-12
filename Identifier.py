66a1393e7f3a1bd3d2c624bcc434102572a99e38
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-12-12 23:35:15 +0530

    Compiler

--- Identifier.py
+++ Identifier.py
@@ -1,8 +1,11 @@
-def identify(input_string):
+import re
+
+def identify(str):
     keywords = ["if", "else", "for", "while", "int", "float", "return", "break"]  
     operators = ["+", "-", "*", "/", "=", "==", "<", ">", "!="] 
+    identifier_pattern = re.compile(r'^[a-zA-Z_]\w*$')
 
-    words = input_string.split()
+    words = str.split()
     num_keywords = 0
     num_identifiers = 0
     num_operators = 0
@@ -12,16 +15,14 @@ def identify(input_string):
             num_keywords += 1
         elif word in operators:
             num_operators += 1
-        else:
+        elif identifier_pattern.match(word):
             num_identifiers += 1
 
     return num_keywords, num_identifiers, num_operators
 
-input_str = "int x = 10; if(x == 5) { x = x + 1; }"
-num_keywords, num_identifiers, num_operators = identify(input_str)
+str = input("Enter A String")
+num_keywords, num_identifiers, num_operators = identify(str)
 
 print("Number of Keywords:", num_keywords)
 print("Number of Identifiers:", num_identifiers)
 print("Number of Operators:", num_operators)
-
-
